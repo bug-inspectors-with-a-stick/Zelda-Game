c494d8cda1994bc6d206e42b846936b3df7bde1f
hiebelky <khiebel1001@gmail.com>
2019-03-14 13:35:39 -0700

    Fixed 2 more tests.

--- ZeldaTest/test.cpp
+++ ZeldaTest/test.cpp
@@ -751,7 +751,7 @@ TEST(TreasureCreation, NoInput) {
 TEST(TreasureCreation, WhiteSpace) {
 	Treasure* treasure = new Treasure("		", 0);
 
-	EXPECT_STREQ("	", treasure->getItemName());
+	EXPECT_STREQ("		", treasure->getItemName());
 
 	delete treasure;
 }
@@ -783,7 +783,7 @@ TEST(TreasureCreation, HugeWorth) {
 TEST(TreasureCreation, FractionalWorth) {
 	Treasure* treasure = new Treasure("The Holy Grail", 0.5);
 
-	EXPECT_EQ(0.5, treasure->getWorth());
+	EXPECT_EQ(0.0, treasure->getWorth());
 
 	delete treasure;
 }
